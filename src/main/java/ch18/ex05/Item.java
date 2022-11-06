fade326ebfc11bee3c5fe434fa91b2efc4375170
motakashi <takashi_home_sky@yahoo.co.jp>
2022-11-06 23:02:23 +0900

    ItemStockクラスのテストコードを追加

--- src/main/java/ch18/ex05/Item.java
+++ src/main/java/ch18/ex05/Item.java
@@ -1,8 +1,8 @@
 package ch18.ex05;
 
 public class Item {
-    public final String name;
-    public final int price;
+    public String name;
+    public int price;
     public Item(String name, int price) {
         this.name = name;
         this.price = price;
--- src/test/java/ch18/ex05/ItemStockTest.java
+++ src/test/java/ch18/ex05/ItemStockTest.java
@@ -3,13 +3,17 @@ package ch18.ex05;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ItemStockTest {
+class ItemStockTest_初回登録の場合 {
+
+    private ItemStock sut;
 
     @BeforeEach
     void setUp() {
+        this.sut = new ItemStock();
     }
 
     @AfterEach
@@ -18,9 +22,38 @@ class ItemStockTest {
 
     @Test
     void add() {
+        // 1. モックオブジェクトの作成
+        Item mockItem = Mockito.mock(Item.class);
+        mockItem.name = "test";
+        mockItem.price = 100;
+
+        this.sut.add(mockItem);
+        assertSame(1, this.sut.getNum(mockItem));
+    }
+}
+
+class ItemStockTest_2回以上の登録の場合 {
+
+    private ItemStock sut;
+
+    @BeforeEach
+    void setUp() {
+        this.sut = new ItemStock();
+    }
+
+    @AfterEach
+    void tearDown() {
     }
 
     @Test
-    void getNum() {
+    void add() {
+        // 1. モックオブジェクトの作成
+        Item mockItem = Mockito.mock(Item.class);
+        mockItem.name = "test";
+        mockItem.price = 100;
+
+        this.sut.add(mockItem);
+        this.sut.add(mockItem);
+        assertSame(2, this.sut.getNum(mockItem));
     }
 }
